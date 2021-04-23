466760079350bc19254b9a49ceb66d00cdb5c81e
Willem Melching <willem.melching@gmail.com>
2021-04-23 17:42:50 +0200

    cleanup

--- selfdrive/ui/qt/widgets/map-native.hpp
+++ selfdrive/ui/qt/widgets/map-native.hpp
@@ -6,53 +6,37 @@
 #include <QPropertyAnimation>
 #include <QScopedPointer>
 #include <QtGlobal>
+#include <QTimer>
 
-class QKeyEvent;
-class QMouseEvent;
-class QWheelEvent;
+#include "messaging.hpp"
 
 class MapWindow : public QOpenGLWidget
 {
     Q_OBJECT
 
 public:
-    MapWindow(const QMapboxGLSettings &);
-    ~MapWindow();
+  MapWindow(const QMapboxGLSettings &);
+  ~MapWindow();
 
-    void selfTest();
-
-protected slots:
-    void animationValueChanged();
-    void animationFinished();
 
 private:
-    void changeStyle();
-    qreal pixelRatio();
+  qreal pixelRatio();
 
-    // QWidget implementation.
-    void keyPressEvent(QKeyEvent *ev) final;
-    void mousePressEvent(QMouseEvent *ev) final;
-    void mouseMoveEvent(QMouseEvent *ev) final;
-    void wheelEvent(QWheelEvent *ev) final;
+  void initializeGL() final;
+  void paintGL() final;
+  QPointF m_lastPos;
 
-    // Q{,Open}GLWidget implementation.
-    void initializeGL() final;
-    void paintGL() final;
+  QMapboxGLSettings m_settings;
+  QScopedPointer<QMapboxGL> m_map;
 
-    QPointF m_lastPos;
+  QVariant m_symbolAnnotationId;
+  QVariant m_lineAnnotationId;
+  QVariant m_fillAnnotationId;
 
-    QMapboxGLSettings m_settings;
-    QScopedPointer<QMapboxGL> m_map;
+  bool m_sourceAdded = false;
+  SubMaster *sm;
+  QTimer* timer;
 
-    QPropertyAnimation *m_bearingAnimation;
-    QPropertyAnimation *m_zoomAnimation;
-
-    unsigned m_animationTicks = 0;
-    unsigned m_frameDraws = 0;
-
-    QVariant m_symbolAnnotationId;
-    QVariant m_lineAnnotationId;
-    QVariant m_fillAnnotationId;
-
-    bool m_sourceAdded = false;
+private slots:
+  void timerUpdate();
 };
